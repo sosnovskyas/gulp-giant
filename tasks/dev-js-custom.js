46f1eaf1090e144ed40ff27da26bd37d3f9c23aa
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2015-10-08 12:49:53 +0300

    refact

--- tasks/dev-js-custom.js
+++ tasks/dev-js-custom.js
@@ -5,10 +5,9 @@ var concat = require('gulp-concat');
 var browserSync  = require('browser-sync');
 
 // developer JS
-gulp.task('dev-js-custom', function () {
-    // custom scripts
-    gulp.src([config.src])
-        .pipe(concat('app.js'))
-        .pipe(gulp.dest(config.dest))
-        .pipe(browserSync.reload({stream: true}));
+gulp.task('dev-js-custom', function() {
+  gulp.src([config.src])
+    .pipe(concat(config.concatFile))
+    .pipe(gulp.dest(config.dest))
+    .pipe(browserSync.reload({stream: true}));
 });
--- tasks/dev.js
+++ tasks/dev.js
@@ -1,37 +1,14 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var config = require('../config').developer;
 
 gulp.task('dev', function(callback) {
-    runSequence(
-        'dev-clean',
-        [
-            'dev-jade',
-            'dev-js-direct',
-            'dev-compass',
-            'dev-fonts',
-            'dev-img'
-        ],
-        'dev-browsersync',
-        [
-            'dev-watch-jade',
-            'dev-watch-compass',
-            'dev-watch-js-custom',
-            'dev-watch-img'
-        ],
-        callback);
+  runSequence(
+    config.sequencePre,
+    config.sequenceClean,
+    config.sequenceBuild,
+    config.sequenceServer,
+    config.sequenceWatch,
+    config.sequencePost,
+    callback);
 });
-/*
-*  НАДО ПЕРЕНЕСТИ НАСТРОЙКУ ПОСЛЕДОВАТЕЛЬНОСТИ В КОНФИГ
-*  в такой конфигурайии почему-то не работает
-*/
-
-//
-//var gulp = require('gulp');
-//var config = require('../config').devMain;
-//var runSequence = require('run-sequence');
-//
-//gulp.task('dev', function (callback) {
-//    runSequence(
-//        config.sequence,
-//        callback);
-//});
